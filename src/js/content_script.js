8b4599f95c5642afc23eaa8e15d679d31fa7589e
Jack Kingsman <jack.kingsman@gmail.com>
2018-01-20 23:04:01 -0800

    Add support for divs

--- src/js/content_script.js
+++ src/js/content_script.js
@@ -13,6 +13,15 @@ chrome.storage.sync.get(null, function(data) {
     }
 });
 
+function moveCursorToEnd(srcElement) {
+    var range = document.createRange();//Create a range (a range is a like the selection but invisible)
+    range.selectNodeContents(srcElement);//Select the entire contents of the element with the range
+    range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+    var selection = window.getSelection();//get the selection object (allows you to change selection)
+    selection.removeAllRanges();//remove any selections already made
+    selection.addRange(range);
+}
+
 /**
  * In the broad strokes, we want to keep track of each keypress by the user.
  * On spacebar check if the last space-surrounded sequence is in our
@@ -22,30 +31,56 @@ chrome.storage.sync.get(null, function(data) {
 **/
 
 var backOut = ''; // hold the text if they want to back out
+var isEditableDiv = false;
+
 function handleKeyPress(event){
     var srcElement = event.srcElement;
-    if(srcElement.type != 'text' && srcElement.type != 'textarea'){
-        return;
+
+    if (srcElement.tagName == "DIV" && srcElement.isContentEditable){
+        isEditableDiv = true;
+    } else {
+        isEditableDiv = false;
+        if(srcElement.type != 'text' && srcElement.type != 'textarea'){ //
+            return;
+        }
     }
 
     if(event.keyCode === 32){
         // spacebar
-        var words = srcElement.value.split(' ');
+        var words;
+        if(isEditableDiv) {
+            words = srcElement.innerHTML.split(' ');
+        } else {
+            words = srcElement.value.split(' ');
+        }
+
         var lastWord = words[words.length - 1];
 
         var replacement = checkReplacement(lastWord);
         if(replacement){
             // we have a match
-            var location = srcElement.value.lastIndexOf(lastWord);
-            backOut = srcElement.value;
-            srcElement.value = srcElement.value.substring(0, location) + replacement;
+            if(isEditableDiv) {
+                console.log(1)
+                var location = srcElement.innerHTML.lastIndexOf(lastWord);
+                backOut = srcElement.innerHTML;
+                srcElement.innerHTML = srcElement.innerHTML.substring(0, location) + replacement;
+                moveCursorToEnd(srcElement);
+            } else {
+                var location = srcElement.value.lastIndexOf(lastWord);
+                backOut = srcElement.value;
+                srcElement.value = srcElement.value.substring(0, location) + replacement;
+            }
         }
 
-    } else if(event.keyCode === 8){
+    } else if(event.keyCode === 8 && backOut){
         // backspace... time to back it out
-        if(backOut){
-            // we have backout text; put it in
-            event.preventDefault(); // we don't actually want that backspace getting through
+        event.preventDefault(); // we don't actually want that backspace getting through
+
+        if(isEditableDiv) {
+            srcElement.innerHTML = backOut + ' ';
+            moveCursorToEnd(srcElement);
+            backOut = null;
+        } else {
             srcElement.value = backOut + ' ';
             backOut = null;
         }
